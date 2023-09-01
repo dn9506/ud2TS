c381b35ae3c5bd16d88d1c8931c6f48abf5b7776
Dmytro Nasekin <dnasekin@gmail.com>
2023-09-01 23:37:56 +0300

    Intermediate save

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -29,11 +29,28 @@ const DrawerNavigation = () => (
 		screenOptions={{
 			...screenOptions,
 			headerShown: false,
+			drawerActiveTintColor: THEME.MAIN_COLOR,
+			drawerLabelStyle: {
+				fontFamily: 'open-bolt',
+				fontSize: 20,
+			},
 		}}
 	>
-		<Drawer.Screen name='MainScreen' component={StackNavigation} />
-		<Drawer.Screen name='CreatePostScreen' component={CreateScreen} />
-		<Drawer.Screen name='AboutScreen' component={AboutScreen} />
+		<Drawer.Screen
+			name='MainScreen'
+			component={StackNavigation}
+			options={{ title: 'Main' }}
+		/>
+		<Drawer.Screen
+			name='CreatePostScreen'
+			component={CreateScreen}
+			options={{ title: 'Create new post', headerShown: true }}
+		/>
+		<Drawer.Screen
+			name='AboutScreen'
+			component={AboutScreen}
+			options={{ title: 'About', headerShown: true }}
+		/>
 	</Drawer.Navigator>
 )
 
@@ -87,74 +104,3 @@ const StackNavigation = () => (
 )
 
 export const AppNavigation = DrawerNavigation
-
-/*
-const DrawerNavigation = () => (
-	<Drawer.Navigator
-		screenOptions={{
-			...screenOptions,
-			headerShown: false,
-		}}
-		initialRouteName='MainScreenRef'
-	>
-		<Drawer.Screen name='MainScreenRef' component={StackNavigation} />
-		<Drawer.Screen
-			name='CreatePostScreen'
-			component={CreateScreen}
-			options={{ headerShown: true }}
-		/>
-		<Drawer.Screen
-			name='AboutScreen'
-			component={AboutScreen}
-			options={{ headerShown: true }}
-		/>
-	</Drawer.Navigator>
-)
-
-const StackNavigation = () => (
-	<Stack.Navigator screenOptions={screenOptions} initialRouteName='Loading'>
-		<Stack.Screen name='MainScreen' component={TabNavigation} />
-		<Stack.Screen
-			name='Loading'
-			component={LoadingScreen}
-			options={LoadingScreen.navigationOptions}
-		/>
-		<Stack.Screen
-			name='Post'
-			component={PostScreen}
-			options={PostScreen.navigationOptions}
-		/>
-	</Stack.Navigator>
-)
-
-const TabNavigation = () => (
-	<Tab.Navigator
-		screenOptions={{
-			...screenOptions,
-			headerShown: false,
-		}}
-	>
-		<Tab.Screen
-			name='AllPosts'
-			component={MainScreen}
-			options={{
-				...MainScreen.navigationOptions,
-				tabBarIcon: info => (
-					<Ionicons name='ios-albums' size={25} color={info.color} />
-				),
-				title: 'All Posts',
-			}}
-		/>
-		<Tab.Screen
-			name='Booked'
-			component={BookedScreen}
-			options={{
-				tabBarIcon: info => (
-					<Ionicons name='ios-star' size={25} color={info.color} />
-				),
-			}}
-		/>
-	</Tab.Navigator>
-)
-
-*/
