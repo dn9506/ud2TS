77f733697ba81d220183cf4f931c89c093af4cee
Dmytro Nasekin <dnasekin@gmail.com>
2023-09-11 23:08:13 +0300

    Intermediate save

--- src/store/reducer/post.ts
+++ src/store/reducer/post.ts
@@ -1,34 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { CaseReducer, createSlice } from '@reduxjs/toolkit'
 import { DATA } from '../../data'
 import { IPost } from '../../models/IPost'
 
 interface IPostState {
 	posts: IPost[]
 }
-
-const initState: IPostState = {
-	posts: DATA,
-}
-
-const store = createSlice({
-	name: 'Post',
-	initialState: initState,
-	reducers: {
-		toggleBooked: (state, action: PostActions) => {
-			state.posts.map(post =>
-				post.id === action.payload.id
-					? (post.booked = !post.booked)
-					: (post.booked = !post.booked)
-			)
-		},
-	},
-})
-
-export default store.reducer
-
 interface postActionToggleBooked {
 	type: 'toggleBooked'
 	payload: IPost
 }
 
 type PostActions = postActionToggleBooked
+
+const initState: IPostState = {
+	posts: DATA,
+}
+
+const toggleBooked: CaseReducer<IPostState, PostActions> = (state, action) => {
+	state.posts.map(post =>
+		post.id === action.payload.id
+			? (post.booked = !post.booked)
+			: (post.booked = !post.booked)
+	)
+}
+
+const store = createSlice({
+	name: 'Post',
+	initialState: initState,
+	reducers: {
+		toggleBooked,
+	},
+})
+
+export default store.reducer
