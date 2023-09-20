984e3c2d4754777ba68d8baf7efb6c126f885076
Dmytro Nasekin <dnasekin@gmail.com>
2023-09-20 23:48:20 +0300

    Intermediate save

--- src/navigation/screens/AllPostsScreen.tsx
+++ src/navigation/screens/AllPostsScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Post from '../../components/Post'
 
 const AllPostsScreen = () => {
 	return (
 		<View>
 			<Text>AllPostsScreen</Text>
+			<Post />
 		</View>
 	)
 }
